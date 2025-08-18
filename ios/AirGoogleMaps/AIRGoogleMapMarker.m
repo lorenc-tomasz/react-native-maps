013e0921ac0005451bbf9e96ca86dbef9609f592
Tomasz Lorenc <lorenc.tomasz@gmail.com>
2025-08-18 09:03:06 +0200

    ios: test snapshoting with auto cache

--- ios/AirGoogleMaps/AIRGoogleMapMarker.m
+++ ios/AirGoogleMaps/AIRGoogleMapMarker.m
@@ -15,6 +15,9 @@
 #import "AIRGoogleMapCallout.h"
 #import "AIRDummyView.h"
 
+// A shared cache for marker snapshots to reuse images and save memory
+static NSCache *markerSnapshotCache;
+
 CGRect unionRect(CGRect a, CGRect b) {
     return CGRectMake(
                       MIN(a.origin.x, b.origin.x),
@@ -24,6 +27,7 @@ CGRect unionRect(CGRect a, CGRect b) {
 }
 
 @interface AIRGoogleMapMarker ()
+- (void)generateSnapshot;
 @end
 
 @implementation AIRGoogleMapMarker {
@@ -45,7 +49,60 @@ CGRect unionRect(CGRect a, CGRect b) {
     NSString* _identifier;
     NSString* _title;
     NSString* _subtitle;
+    BOOL _snapshot;
+    BOOL _snapshotDirty;
+}
 
+// Singleton accessor for the shared cache
++ (NSCache *)sharedMarkerSnapshotCache {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        markerSnapshotCache = [[NSCache alloc] init];
+        markerSnapshotCache.countLimit = 100; // Limit cache size to 100 unique marker images
+    });
+    return markerSnapshotCache;
+}
+
+// MODIFIED: Added a fallback for non-UIColor objects (like SVG brushes) to prevent crashes.
+- (void)generateCacheKeyForView:(UIView *)view withKey:(NSMutableString *)key {
+    // Include class, frame, background color, and alpha for all views
+    [key appendFormat:@"%@-%@;", NSStringFromClass([view class]), NSStringFromCGRect(view.frame)];
+    [key appendFormat:@"bg:%@-a:%.2f;", [self stringFromColor:view.backgroundColor], view.alpha];
+
+    // Add view-specific properties for standard components
+    if ([view isKindOfClass:[UIImageView class]]) {
+        UIImageView *imageView = (UIImageView *)view;
+        [key appendFormat:@"img:%p-tint:%@;", imageView.image, [self stringFromColor:imageView.tintColor]];
+    } else if ([view isKindOfClass:[UILabel class]]) {
+        UILabel *label = (UILabel *)view;
+        [key appendFormat:@"txt:%@-font:%@-color:%@;", label.text, label.font, [self stringFromColor:label.textColor]];
+    }
+
+    // --- MODIFIED: SVG-specific property checking with crash protection ---
+    if ([view respondsToSelector:NSSelectorFromString(@"fill")]) {
+        id fillValue = [view valueForKey:@"fill"];
+        if ([fillValue isKindOfClass:[UIColor class]]) {
+            [key appendFormat:@"fill:%@;", [self stringFromColor:(UIColor *)fillValue]];
+        } else if (fillValue) {
+            // Fallback for non-UIColor objects like RNSVGSolidColorBrush by using its description.
+            [key appendFormat:@"fill-desc:%@;", [fillValue description]];
+        }
+    }
+    if ([view respondsToSelector:NSSelectorFromString(@"stroke")]) {
+        id strokeValue = [view valueForKey:@"stroke"];
+        if ([strokeValue isKindOfClass:[UIColor class]]) {
+            [key appendFormat:@"stroke:%@;", [self stringFromColor:(UIColor *)strokeValue]];
+        } else if (strokeValue) {
+            // Fallback for non-UIColor objects like RNSVGSolidColorBrush.
+            [key appendFormat:@"stroke-desc:%@;", [strokeValue description]];
+        }
+    }
+    // --- END SVG LOGIC ---
+
+    // Recurse for subviews
+    for (UIView *subview in view.subviews) {
+        [self generateCacheKeyForView:subview withKey:key];
+    }
 }
 
 - (instancetype)init
@@ -55,11 +112,14 @@ CGRect unionRect(CGRect a, CGRect b) {
         _tracksInfoWindowChanges = false;
         _tappable = true;
         _opacity = 1.0;
+        _snapshot = NO;
+        _snapshotDirty = NO;
     }
     return self;
 }
 
 - (void)layoutSubviews {
+    [super layoutSubviews];
     float width = 0;
     float height = 0;
 
@@ -72,7 +132,16 @@ CGRect unionRect(CGRect a, CGRect b) {
         height = MAX(fh, height);
     }
 
-    [_iconView setFrame:CGRectMake(0, 0, width, height)];
+    CGRect newFrame = CGRectMake(0, 0, width, height);
+
+    if (_iconView && !CGRectEqualToRect(_iconView.frame, newFrame)) {
+        [_iconView setFrame:newFrame];
+        _snapshotDirty = YES;
+    }
+
+    if (_snapshot && _snapshotDirty) {
+        [self generateSnapshot];
+    }
 }
 
 
@@ -81,11 +150,64 @@ CGRect unionRect(CGRect a, CGRect b) {
     return _iconView;
 }
 
+// ADDED: Helper to convert UIColor to a string representation for the cache key.
+- (NSString *)stringFromColor:(UIColor *)color {
+    if (!color) return @"nil";
+    CGFloat r, g, b, a;
+    [color getRed:&r green:&g blue:&b alpha:&a];
+    return [NSString stringWithFormat:@"%.3f,%.3f,%.3f,%.3f", r, g, b, a];
+}
+
+- (void)generateSnapshot {
+    if (!_snapshot || !_snapshotDirty || !_realMarker || !_iconView) return;
+
+    if (CGSizeEqualToSize(_iconView.bounds.size, CGSizeZero)) {
+        return;
+    }
+
+    // --- START CACHING LOGIC ---
+    NSMutableString *cacheKeyBuilder = [NSMutableString new];
+    [self generateCacheKeyForView:_iconView withKey:cacheKeyBuilder];
+    NSString *cacheKey = [NSString stringWithString:cacheKeyBuilder];
+
+    UIImage *cachedImage = [[[self class] sharedMarkerSnapshotCache] objectForKey:cacheKey];
+
+    if (cachedImage) {
+        _realMarker.icon = cachedImage;
+        _realMarker.iconView = nil;
+        _realMarker.tracksViewChanges = NO;
+        _snapshotDirty = NO;
+        return; // Cache hit, no need to generate a new image
+    }
+    // --- END CACHING LOGIC ---
+
+    UIGraphicsBeginImageContextWithOptions(_iconView.bounds.size, NO, 0.0);
+    [_iconView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *snapshotImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    if (snapshotImage) {
+        // Store the newly generated image in the cache.
+        [[[self class] sharedMarkerSnapshotCache] setObject:snapshotImage forKey:cacheKey];
+
+        _realMarker.icon = snapshotImage;
+        _realMarker.iconView = nil;
+        _realMarker.tracksViewChanges = NO;
+    }
+
+    _snapshotDirty = NO;
+}
+
 - (void) didUpdateReactSubviews
 {
     [super didUpdateReactSubviews];
     if (_iconView){
         [_iconView setFrame:self.frame];
+    }
+
+    if (_snapshot) {
+        _snapshotDirty = YES;
+        [self setNeedsLayout];
     }
 }
 
@@ -93,13 +215,9 @@ CGRect unionRect(CGRect a, CGRect b) {
 {
     _realMarker = [AIRGMSMarker new];
     _realMarker.fakeMarker = self;
-    _realMarker.tracksViewChanges = _tracksViewChanges;
     _realMarker.tracksInfoWindowChanges = _tracksInfoWindowChanges;
 
     [_realMarker setPosition:_coordinates];
-    if (_iconView){
-        [_realMarker setIconView:_iconView];
-    }
     if (_rotation != 0){
         [_realMarker setRotation:_rotation];
     }
@@ -125,9 +243,6 @@ CGRect unionRect(CGRect a, CGRect b) {
         [_realMarker setDraggable:_draggable];
     }
     [_realMarker setTappable:_tappable];
-    if (_pinColor){
-        _realMarker.icon = [GMSMarker markerImageWithColor:_pinColor];
-    }
     if (_opacity != 1.0){
         [_realMarker setOpacity:_opacity];
     }
@@ -143,6 +258,19 @@ CGRect unionRect(CGRect a, CGRect b) {
     if (_zIndex){
         [_realMarker setZIndex:_zIndex];
     }
+
+    if (_snapshot) {
+        _snapshotDirty = YES;
+        [self generateSnapshot];
+    } else {
+        if (_iconView) {
+            _realMarker.iconView = _iconView;
+        } else if (_pinColor) {
+            _realMarker.icon = [GMSMarker markerImageWithColor:_pinColor];
+        }
+        _realMarker.tracksViewChanges = _tracksViewChanges;
+    }
+
     [_realMarker setMap:map];
 }
 
@@ -150,16 +278,17 @@ CGRect unionRect(CGRect a, CGRect b) {
     if (!_iconView){
         _iconView = [[UIView alloc] init];
     }
-    if (!_realMarker.iconView) {
+    if (!_realMarker.iconView && !_snapshot) {
         _realMarker.iconView = _iconView;
     }
     [_iconView insertSubview:subview atIndex:atIndex];
+    _snapshotDirty = YES;
 }
 
 - (void)insertReactSubview:(id<RCTComponent>)subview atIndex:(NSInteger)atIndex {
     if ([subview isKindOfClass:[AIRGoogleMapCallout class]]) {
         self.calloutView = (AIRGoogleMapCallout *)subview;
-    } else { // a child view of the marker
+    } else {
         [self iconViewInsertSubview:(UIView*)subview atIndex:atIndex+1];
     }
     AIRDummyView *dummySubview = [[AIRDummyView alloc] initWithView:(UIView *)subview];
@@ -173,6 +302,7 @@ CGRect unionRect(CGRect a, CGRect b) {
         self.calloutView = nil;
     } else {
         [subview removeFromSuperview];
+        _snapshotDirty = YES;
     }
     [super removeReactSubview:(UIView*)dummySubview];
 }
@@ -186,20 +316,24 @@ CGRect unionRect(CGRect a, CGRect b) {
 }
 
 - (void)redraw {
+    if (_snapshot) {
+        _snapshotDirty = YES;
+        [self generateSnapshot];
+        return;
+    }
+
     if (!_realMarker.iconView) return;
 
     BOOL oldValue = _realMarker.tracksViewChanges;
 
     if (oldValue == YES)
     {
-        // Immediate refresh, like right now. Not waiting for next frame.
         UIView *view = _realMarker.iconView;
         _realMarker.iconView = nil;
         _realMarker.iconView = view;
     }
     else
     {
-        // Refresh according to docs
         _realMarker.tracksViewChanges = YES;
         _realMarker.tracksViewChanges = NO;
     }
@@ -469,7 +603,9 @@ CGRect unionRect(CGRect a, CGRect b) {
 
 - (void)setPinColor:(UIColor *)pinColor {
     _pinColor = pinColor;
-    _realMarker.icon = [GMSMarker markerImageWithColor:pinColor];
+    if (!_iconView && !_snapshot) {
+        _realMarker.icon = [GMSMarker markerImageWithColor:pinColor];
+    }
 }
 
 - (void)setAnchor:(CGPoint)anchor {
@@ -517,7 +653,10 @@ CGRect unionRect(CGRect a, CGRect b) {
 }
 
 - (void)setTracksViewChanges:(BOOL)tracksViewChanges {
-    _realMarker.tracksViewChanges = tracksViewChanges;
+    _tracksViewChanges = tracksViewChanges;
+    if (!_snapshot) {
+        _realMarker.tracksViewChanges = tracksViewChanges;
+    }
 }
 
 - (BOOL)tracksViewChanges {
@@ -532,23 +671,45 @@ CGRect unionRect(CGRect a, CGRect b) {
     return _realMarker.tracksInfoWindowChanges;
 }
 
+- (void)setSnapshot:(BOOL)snapshot {
+    if (_snapshot == snapshot) return;
+    _snapshot = snapshot;
+    _snapshotDirty = YES;
+
+    if (_snapshot) {
+        [self generateSnapshot];
+    } else {
+        // Revert to non-snapshot mode
+        if (_iconView) {
+            _realMarker.icon = nil;
+            _realMarker.iconView = _iconView;
+            _realMarker.tracksViewChanges = _tracksViewChanges;
+        } else if (_pinColor) {
+            _realMarker.icon = [GMSMarker markerImageWithColor:_pinColor];
+            _realMarker.iconView = nil;
+        } else {
+            _realMarker.icon = nil;
+            _realMarker.iconView = nil;
+        }
+    }
+}
 
 - (id)makeEventData:(NSString *)action {
     CLLocationCoordinate2D coordinate = self.realMarker.position;
     CGPoint position = [self.realMarker.map.projection pointForCoordinate:coordinate];
 
     return @{
-        @"id": self.identifier ?: @"unknown",
-        @"position": @{
-            @"x": @(position.x),
-            @"y": @(position.y),
-        },
-        @"coordinate": @{
-            @"latitude": @(coordinate.latitude),
-            @"longitude": @(coordinate.longitude),
-        },
-        @"action": action,
-    };
+             @"id": self.identifier ?: @"unknown",
+             @"position": @{
+                     @"x": @(position.x),
+                     @"y": @(position.y),
+                     },
+             @"coordinate": @{
+                     @"latitude": @(coordinate.latitude),
+                     @"longitude": @(coordinate.longitude),
+                     },
+             @"action": action,
+             };
 }
 
 - (id)makeEventData {
